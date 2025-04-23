ec933d22e3a9f7b9ddff498d9e3e8bc228edec72
Ramananda naik <vn.sumana@gmail.com>
2025-04-23 13:07:49 -0700

    1.Modify to add no. of attempts

--- Number_guessing_game.py
+++ Number_guessing_game.py
@@ -5,7 +5,7 @@
 
 import random
 
-
+# Function to check if user has given valid input.
 def is_valid_input(input_text):
     if input_text.isdigit():
         return True
@@ -14,7 +14,8 @@ def is_valid_input(input_text):
 
 
 print("---Welcome the game of Number guessing."
-      "Input your lower and upper range to play the guessing game.---\n")
+      "Input your lower and upper range to play the guessing game\n"
+      "You have 7 chances to guess. Good Luck!!---\n")
 
 # check input validitity
 start = input("Enter Start range: ")
@@ -28,23 +29,28 @@ else:
         start_range = int(start)
         end_range = int(end)
 
-
+        no_of_chances = 7 # user has to guess within 7 chances
         number_to_be_guessed = random.randint(start_range,end_range)
         no_of_guesses = 0  # counter to keep track of guesses
 
         print("\n.......The Guessing Game Begins now........")
 
         while True:
+            if no_of_guesses == no_of_chances:
+                print(f"Couldn't guess in {no_of_chances} turns. \nBetter Luck new time...")
+                break
+
             guessed_num = int(input("\nGuess the number: "))
             no_of_guesses += 1
 
             if guessed_num == number_to_be_guessed:
-                print(f"\nCongratulations! \nTotal number of guesses = {no_of_guesses}.")
+                print(f"\nCongratulations!  You Got it!! \nTotal number of guesses = {no_of_guesses}.")
                 break
             else:
                 if guessed_num > number_to_be_guessed:
-                    print("Try again! You guessed too high.")
+                    print(f"\nTry again.. {no_of_chances-no_of_guesses} turns Left ! \n.... CLUE: You guessed too high.")
                 else:
-                    print("Try again! You guessed too small.")
+                    print(f"\nTry again.. {no_of_chances-no_of_guesses} turns Left! \n.... CLUE: You guessed too small.")
+
 
 
